ea8cc7a02dfd65325dcc1b01cc3cbbadae583138
ugo-quaisse <54071701+ugo-quaisse@users.noreply.github.com>
2020-05-10 00:33:00 +0200

    Release 2.1

--- js/modules/snackbar.js
+++ js/modules/snackbar.js
@@ -10,8 +10,8 @@ export {showSnackbar};
 const showSnackbar = (version) => {
 
     //Snackbar settings
-    var snackbarHtml = "<b>Welcome to Sitecore Author Toolbox 2.0</b><br />You can now setup your CM or CD/Live domains to get better Live URL, clic the Settings button.";        
-    var html='<div class="snackbar"> ' + snackbarHtml + ' <button onclick="window.open(\' ' + global.launchpadPage + '?configure_domains=false&launchpad=true&url= ' + global.windowLocationHref + ' &tabs=0 \')">SETTINGS</button><button id="sbDismiss">DISMISS</button></div>';
+    var snackbarHtml = "<b>Welcome to Sitecore Author Toolbox 2.1</b><br />You've got upgrated to the latest version. If you like it, please take time to rate it on Chrome Web Store.";        
+    var html='<div class="snackbar"> ' + snackbarHtml + ' <button onclick="window.open(\' https://chrome.google.com/webstore/detail/sitecore-author-toolbox/mckfcmcdbgkgffdknpkaihjigmcnnbco \')">REVIEW</button><button id="sbDismiss">DISMISS</button></div>';
 
     //Show Snackbar
     document.querySelector('body').insertAdjacentHTML( 'beforeend', html );

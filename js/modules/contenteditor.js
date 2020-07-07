7ea5dc8b5703342145756949a95a00bc52d86ad0
ugo-quaisse <54071701+ugo-quaisse@users.noreply.github.com>
2020-07-07 08:33:51 +0200

    Copy to clipboard update

--- js/modules/contenteditor.js
+++ js/modules/contenteditor.js
@@ -464,7 +464,7 @@ const sitecoreAuthorToolbox = () => {
 
                     field.setAttribute('style', 'width: calc(100%-16px); margin-left:2px; display: none');
                     field.classList.add("copyCount_"+copyCount);
-                    copyHtml = `<span class="copyCountSpan_` + copyCount + `">` + field.value + `</span> <a class="t-top t-sm" data-tooltip="Copy" onclick="copyContent('` + field.value + `', '` + copyCount + `')"><img src="` + global.iconCopy + `" class="scIconCopy" /></a> <span class="copyCountMessage_` + copyCount + `"></span>`
+                    copyHtml = `<span onclick="copyContent('` + field.value + `', '` + copyCount + `')" class="copyCountSpan_` + copyCount + `">` + field.value + `</span> <a class="t-top t-sm" data-tooltip="Copy" onclick="copyContent('` + field.value + `', '` + copyCount + `')"><img src="` + global.iconCopy + `" class="scIconCopy" /></a> <span class="copyCountMessage_` + copyCount + `"></span>`
                     field.value != "[unknown]" ? field.insertAdjacentHTML('beforebegin', copyHtml) : field.insertAdjacentHTML('beforebegin', `<span>` + field.value + `</span>`);
                     copyCount++;
 

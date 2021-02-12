2aced2cc33796f346a5b551cd3aeebad506d3a32
ugo-quaisse <54071701+ugo-quaisse@users.noreply.github.com>
2021-02-13 00:19:56 +0100

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/* eslint-disable array-element-newline */
+/* eslint-disable newline-per-chained-call */
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-undef */
+/* eslint-disable func-style */
+const gulp = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const wait = require("gulp-wait");
+const open = require("gulp-open");
+const livereload = require("gulp-livereload");
+// Task to minify css using package cleanCSs
+gulp.task("minify-css", function () {
+  return gulp
+    .src("css/main.css")
+    .pipe(cleanCSS())
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("css/"))
+    .pipe(wait(500))
+    .pipe(open({ uri: "http://reload.extensions" }));
+  //https://chrome.google.com/webstore/detail/extensions-reloader/fimgfedafeadlieiabdeeaodndnlbhid
+});
+gulp.task("watch", function () {
+  gulp.watch(["css/*.css", "!css/*.min.css"], gulp.series(["minify-css"]));
+});
+
+//Default
+gulp.task("default", gulp.series(["watch"]));

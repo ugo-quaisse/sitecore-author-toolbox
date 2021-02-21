3cbc74251cddaee442a21eda6b085062ecfeb639
ugo-quaisse <54071701+ugo-quaisse@users.noreply.github.com>
2021-02-21 23:11:57 +0100

    Fix linter

---
+++ js/modules/local.js
@@ -0,0 +1,7 @@
+/* eslint no-console: ["error", { allow: ["warn", "error", "log", "info", "table", "time", "timeEnd"] }] */
+
+/**
+ * Global variables declaration
+ */
+export const fullcontactApiKey = "";
+export const googleApiKey = "";
